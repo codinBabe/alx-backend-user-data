4c9139b40fe6d6d5e2f87cde16f51a0cf5b64806
codinBabe <teboola2@gmail.com>
2024-09-13 05:48:00 +0100

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,8 @@ from typing import List
 import logging
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """returns the log message obfuscated"""
     return re.sub(r"(?<={}=).*?(?={})".format(fields[0], separator),
                   redaction, message)
@@ -31,4 +32,3 @@ class RedactingFormatter(logging.Formatter):
             """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
-    
