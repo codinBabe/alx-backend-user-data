87579d1773a3f2e5399154c014d4fb2d7aca773c
codinBabe <teboola2@gmail.com>
2024-09-13 05:49:41 +0100

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,7 +26,6 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-
     def format(self, record: logging.LogRecord) -> str:
         """ Format method
             """
