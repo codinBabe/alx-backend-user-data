9272d3cbc86adda35602a5c2d29f048caab002e2
codinBabe <teboola2@gmail.com>
2024-09-13 05:33:39 +0100

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Regex-ing"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    return re.sub(r"(?<={}=).*?(?={})".format(fields[0], separator),
+                  redaction, message)
