9d4039523f76bc4364f6db378dde576b5e55c8da
codinBabe <teboola2@gmail.com>
2024-09-14 16:37:48 +0100

    completed:task 5-6

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+import bcrypt
+""" Encrypting passwords """
+
+
+def hash_password(password: str) -> bytes:
+    """ Encrypting passwords """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validate that the provided password matches the hashed password """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
