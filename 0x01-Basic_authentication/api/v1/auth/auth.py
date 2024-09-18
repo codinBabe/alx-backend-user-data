e164643d6aa10c9710d11484fb12e15e5c50e850
codinBabe <teboola2@gmail.com>
2024-09-18 16:32:18 +0100

    completed: task 7-8

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ Module of API Authentification"""
-import re
 from flask import request
 from typing import List, TypeVar
 
@@ -9,25 +8,20 @@ class Auth:
     """ Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth method"""
-        if path is not None and excluded_paths is not None:
-            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
-                    return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header method"""
-        if request is None or 'Authorization' not in request.headers:
-            return None
-        return request.headers['Authorization']
-
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+    
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user method"""
         return None
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,40 @@
 #!/usr/bin/env python3
 """ Module of API Authentification"""
+import re
+import base64
+import binascii
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    pass
+    """ BasicAuth class"""
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str,
+            ) -> str:
+        """Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
