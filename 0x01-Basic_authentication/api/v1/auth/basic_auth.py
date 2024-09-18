a5d0b93c61543b2ccf3a5427ea6ea84956a16278
codinBabe <teboola2@gmail.com>
2024-09-18 17:00:27 +0100

    rm db instance

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,11 +60,28 @@ class BasicAuth(Auth):
         """
         if type(user_email) == str and type(user_pwd) == str:
             try:
-                user = User.search({'email': user_email})
+                users = User.search({'email': user_email})
             except Exception:
                 return None
-            if len(user) == 0:
+            if len(users) <= 0:
                 return None
-            if not user[0].is_valid_password(user_pwd):
-                return user[0]
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Overloads Auth and retrieves the User instance for a request.
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        if base64_header is None:
+            return None
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        if decoded_header is None:
+            return None
+        user, pwd = self.extract_user_credentials(decoded_header)
+        if user is None or pwd is None:
+            return None
+        return self.user_object_from_credentials(user, pwd)
