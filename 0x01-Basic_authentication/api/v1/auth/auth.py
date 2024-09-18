e8ee747428bec481f792b1f46ca8d93e985377c6
codinBabe <teboola2@gmail.com>
2024-09-18 16:35:33 +0100

    completed: task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,12 +8,17 @@ class Auth:
     """ Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth method"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
@@ -21,7 +26,7 @@ class Auth:
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user method"""
         return None
