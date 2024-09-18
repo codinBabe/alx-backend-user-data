f9bc0c93affc2abd204d7f1cf1b33885b96566c9
codinBabe <teboola2@gmail.com>
2024-09-18 15:57:24 +0100

    completed: task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,8 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth_type = getenv("AUTH_TYPE")
+auth = None
+auth_type = getenv("AUTH_TYPE", "auth")
 if auth_type == "basic_auth":
     auth = BasicAuth()
 else:
