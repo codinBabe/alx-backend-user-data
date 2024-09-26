b7b381b3901ba220a277c265c4cb8be1fdd864a2
codinBabe <teboola2@gmail.com>
2024-09-26 17:53:46 +0100

    completed: task 7-8

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Flask app module"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
-
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,6 +15,22 @@ def hello() -> str:
       - welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """POST /users
+    Register a user
+    Return:
+      - user object
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,14 +4,14 @@
 
 from user import User
 from db import DB
-from bcrypt import hashpw, gensalt
+import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
     """Hash password
         """
-    return hashpw(password.encode(), gensalt())
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
 class Auth:
@@ -29,3 +29,17 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
+    
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login
+        """
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                return bcrypt.checkpw(
+                    password.encode('utf-8'),
+                    user.hashed_password)
+        except NoResultFound:
+            return False
+        return False
